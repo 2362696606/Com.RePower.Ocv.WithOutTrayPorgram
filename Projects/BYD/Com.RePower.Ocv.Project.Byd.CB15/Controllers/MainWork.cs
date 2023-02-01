e4bb724a8eba4cf9895c528549d123e2256999f7
唐勤政 <2362696606@qq.com>
2023-02-01 10:17:02 +0800

    添加CB15项目

---
+++ Projects/BYD/Com.RePower.Ocv.Project.Byd.CB15/Controllers/MainWork.cs
@@ -0,0 +1,18 @@
+﻿using Com.RePower.Ocv.Project.ProjectBase.Controllers;
+using Com.RePower.WpfBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.RePower.Ocv.Project.Byd.CB15.Controllers
+{
+    public class MainWork : MainWorkAbstract
+    {
+        protected override OperateResult DoWork()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Uis/BYD/Com.RePower.Ocv.Ui.Byd.CB15/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Autofac;
+using Com.RePower.Ocv.Model.DataBaseContext;
+using Com.RePower.Ocv.Model.Mapper;
+using Com.RePower.Ocv.Ui.UiBase;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Com.RePower.Ocv.Ui.Byd.CB15
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : UiBaseApplication
+    {
+        protected override void AddService(ServiceCollection serviceCollection)
+        {
+            serviceCollection.AddAutoMapper(typeof(OrganizationProfile));
+            //serviceCollection.AddHttpClient();
+            serviceCollection.AddDbContext<LocalTestResultDbContext>();
+        }
+
+        protected override void IocRegister(ContainerBuilder builder)
+        {
+            
+        }
+    }
+}
