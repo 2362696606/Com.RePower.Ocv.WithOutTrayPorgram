889399bd615e385ec317a6de376a1c05c3acecd8
唐勤政 <2362696606@qq.com>
2023-06-07 11:28:45 +0800

    CB09校准生成表格

--- Projects/BYD/Com.RePower.Ocv.Project.Byd.CB09/Works/MasterTestWork.cs
+++ Projects/BYD/Com.RePower.Ocv.Project.Byd.CB09/Works/MasterTestWork.cs
@@ -1,10 +1,12 @@
 ﻿using System.Diagnostics.Eventing.Reader;
+using System.Globalization;
 using Com.RePower.Device.Plc.Impl;
 using Com.RePower.Ocv.Project.Byd.CB09.Messages;
 using Com.RePower.Ocv.Project.Byd.CB09.Settings;
 using Com.RePower.WpfBase;
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
+using Npoi.Mapper;
 
 namespace Com.RePower.Ocv.Project.Byd.CB09.Works;
 
@@ -19,8 +21,17 @@ public partial class MainWork
         {
             return;
         }
+
+        List<CalibrationExcelDto> dtos = new List<CalibrationExcelDto>();
         foreach (var item in calibrationSetting.CalibrationItems)
         {
+            CalibrationExcelDto dto = new CalibrationExcelDto
+            {
+                Channel = item.Channel.ToString(),
+                StandRes = item.StandRes.ToString(CultureInfo.InvariantCulture),
+                AutoCalibrationValue = item.AutoCalibrationValue.ToString(CultureInfo.InvariantCulture),
+                ManualCalibrationValue = item.ManualCalibrationValue.ToString(CultureInfo.InvariantCulture)
+            };
             CloseAllChannel();
             int channel = item.Channel;
             OpenChannelByBattery(channel);
@@ -37,9 +48,8 @@ public partial class MainWork
                 DoMethodMessage message = new DoMethodMessage()
                 {
                     MethodName = "CalibrationChange",
-                    Parameters = new Dictionary<string, object>(),
+                    Parameters = new Dictionary<string, object> { { "ChangeItem", param } },
                 };
-                message.Parameters.Add("ChangeItem", param);
                 WeakReferenceMessenger.Default.Send<DoMethodMessage, string>(message,
                     "CalibrationChangeMessage");
             }
@@ -48,6 +58,8 @@ public partial class MainWork
                 var readValue = readResult.Content;
                 double subValue = (Com.RePower.Ocv.Model.Extensions.DoubleExtensions.Sub(item.StandRes, readValue) ??
                                    throw new Exception("计算异常"));
+                dto.ReadValue = readValue.ToString(CultureInfo.InvariantCulture);
+                dto.SubValue = subValue.ToString(CultureInfo.InvariantCulture);
                 item.AutoCalibrationValue = subValue;
                 calibrationSetting.SaveChanged();
                 CalibrationChangedMessage param = new CalibrationChangedMessage()
@@ -59,14 +71,30 @@ public partial class MainWork
                 DoMethodMessage message = new DoMethodMessage()
                 {
                     MethodName = "CalibrationChange",
-                    Parameters = new Dictionary<string, object>(),
+                    Parameters = new Dictionary<string, object> { { "ChangeItem", param } },
                 };
-                message.Parameters.Add("ChangeItem", param);
                 WeakReferenceMessenger.Default.Send<DoMethodMessage, string>(message,
                     "CalibrationChangeMessage");
             }
+            dtos.Add(dto);
         }
-        
+        Npoi.Mapper.Mapper mapper = new Npoi.Mapper.Mapper();
+        string fileName = DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss") + ".xlsx";
+        //string fileName = DateTime.Now.ToString("yyyy/MM/dd-HH:mm:ss") + ".xlsx";
+        string dirPath = Path.GetFullPath("./Master");
+        string fullPath = Path.Combine(dirPath, fileName);
+        if (!Directory.Exists(dirPath))
+        {
+            Directory.CreateDirectory(dirPath);
+        }
+
+        mapper.Map<CalibrationExcelDto>("通道", s => s.Channel)
+            .Map<CalibrationExcelDto>("自动校准值", s => s.AutoCalibrationValue)
+            .Map<CalibrationExcelDto>("手动校准值", s => s.ManualCalibrationValue)
+            .Map<CalibrationExcelDto>("标准值", s => s.StandRes)
+            .Map<CalibrationExcelDto>("读取值", s => s.ReadValue)
+            .Map<CalibrationExcelDto>("偏差值", s => s.SubValue);
+        mapper.Save(fullPath, dtos, "sheet1", true, true);
     }
     public virtual bool CanDoMasterTest()
     {
@@ -76,4 +104,14 @@ public partial class MainWork
         }
         return false;
     }
+}
+
+public class CalibrationExcelDto
+{
+    public string Channel { get; set; } = "UnKnown";
+    public string AutoCalibrationValue { get; set; } = "UnKnown";
+    public string ManualCalibrationValue { get; set; } = "UnKnown";
+    public string StandRes { get; set; } = "UnKnown";
+    public string ReadValue { get; set; } = "UnKnown";
+    public string SubValue { get; set; } = "UnKnown";
 }
